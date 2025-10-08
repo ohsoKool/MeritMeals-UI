229eb93f7644f04f372b4748ad9143789266af2b
ohsoKool <rishikeshgunda9@gmail.com>
2025-10-08 20:34:01 +0530

    Initial Commit

---
+++ MeritMeals/MeritMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeritMealsApp.swift
+//  MeritMeals
+//
+//  Created by Rishikesh Gunda on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MeritMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
